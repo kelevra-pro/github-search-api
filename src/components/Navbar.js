2bedb3c8984d45c558e31362bf35b99084735fbb
Mukhamedinov <damtat8@gmail.com>
2020-08-08 13:29:00 +0300

    add routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
@@ -7,10 +8,10 @@ export const Navbar = () => (
     </div>
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a href="/" className="nav-link">Main</a>
+        <NavLink exact to="/" className="nav-link">Main</NavLink>
       </li>
       <li className="nav-item">
-        <a href="/info" className="nav-link">Info</a>
+        <NavLink to="/about" className="nav-link">Info</NavLink>
       </li>
     </ul>
   </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="jumbotron ">
+      <div className="container">
+        <h1 className="display-4">I nformation</h1>
+        <p className="lead">Version <strong>1.0.0</strong></p>
+      </div>
     </div>
   );
 };
