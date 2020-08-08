5a98a47875a6a702935cee1e4c7e0fe0632698fe
Mukhamedinov <damtat8@gmail.com>
2020-08-08 13:30:41 +0300

    fix about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div className="jumbotron ">
       <div className="container">
-        <h1 className="display-4">I nformation</h1>
+        <h1 className="display-4">Information</h1>
         <p className="lead">Version <strong>1.0.0</strong></p>
       </div>
     </div>
