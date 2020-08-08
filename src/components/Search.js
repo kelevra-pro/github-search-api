1843fa231437c96f53522c5c1a7be8b4e42213f2
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2020-08-08 13:59:15 +0300

    main page (#7)

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Search = () => {
+  return (
+    <div className="form-group">
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter the user's nickname..."
+      />
+    </div>
+  );
+};
