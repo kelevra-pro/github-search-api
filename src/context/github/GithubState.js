fa3a224c984790a25cffabe0cfc3789a7fba21db
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2020-08-20 00:44:44 +0300

    add github search api (#14)
    * add github state functions
    
    * add github search api

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import { GithubContext } from './githubContext';
 import { githubReducer } from './githubReducer';
 import {
@@ -8,6 +9,9 @@ import {
   SEARCH_USERS,
   SET_LOADING
 } from '../types';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
 export const GithubState = ({ children }) => {
   const initialState = {
@@ -21,11 +25,14 @@ export const GithubState = ({ children }) => {
 
   const search = async value => {
     setLoading();
-    //...
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    );
 
     dispatch({
       type: SEARCH_USERS,
-      payload: []
+      payload: response.data.items
     });
   };
 
