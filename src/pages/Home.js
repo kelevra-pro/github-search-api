57897dcf2ef302d5043c38d95423c49d1844c5b4
Damir Mukhamedinov <56725080+Kelevra-pro@users.noreply.github.com>
2020-08-30 14:22:10 +0300

    add user show (#18)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export const Home = () => {
       <div className="row">
 
         {loading
-          ? <p className="text-center">Загрузка...</p>
+          ? <p className="text-center">Loading...</p>
           : users.map(user => (
             <div className="col-sm-4 mb-4" key={user.id}>
               <Card user={user}/>
